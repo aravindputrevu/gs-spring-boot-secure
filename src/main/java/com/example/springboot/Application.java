0472bbcb011e76e8f5ea506061a35424dd71d8b7
aravindputrevu <aravind.putrevu@elastic.co>
2020-05-21 20:46:22 +0530

    elastic apm changes

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -9,12 +9,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import co.elastic.apm.attach.ElasticApmAttacher;
 
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+        ElasticApmAttacher.attach();
 		SpringApplication.run(Application.class, args);
 	}
 
